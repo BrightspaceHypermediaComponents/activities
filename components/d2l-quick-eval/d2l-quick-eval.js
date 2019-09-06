7696dc024e342b98402c024686a3abe6ca1e392a
Viktor Yakovlyev <viktor.y@d2l.com>
2019-09-06 16:30:58 -0400

    add mixin behaviours and tememetry behaviour

--- components/d2l-quick-eval/d2l-quick-eval.js
+++ components/d2l-quick-eval/d2l-quick-eval.js
@@ -1,5 +1,6 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {QuickEvalLocalize} from './QuickEvalLocalize.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import 'd2l-typography/d2l-typography-shared-styles.js';
 import './d2l-quick-eval-view-toggle.js';
 import './d2l-quick-eval-activities.js';
@@ -10,7 +11,13 @@ import './behaviors/d2l-quick-eval-telemetry-behavior.js';
  * @customElement
  * @polymer
  */
-class D2LQuickEval extends  QuickEvalLocalize(PolymerElement) {
+class D2LQuickEval extends
+	mixinBehaviors (
+		[
+			D2L.PolymerBehaviors.QuickEval.TelemetryBehaviorImpl
+		],
+		QuickEvalLocalize(PolymerElement)
+	) {
 	static get template() {
 		return html`
 			<style>

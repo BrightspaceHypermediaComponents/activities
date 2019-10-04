0c2a7d0e202166c8455f811b328b3392c2080447
Ryan Mahler <ryan.mahler@d2l.com>
2019-10-04 14:37:13 -0700

    US110605 Followup: Serge run_command
    Based on the Serge docs, there's a `run_command` plugin with which you can run arbitrary commands. This adds some commands to build our language files for us, so that (in theory) Serge will return to us both the translated JSON files, as well as built JavaScript files, which we can then import. Importing these by name allows the BSI analyzer to actually include the files correctly - we load only the files we need initially, and lazy-load the others if they're needed (e.g. if the page language changes).
    
    I say "in theory" above as I've not been able to actually test this on the Serge service itself - the commands work as expected locally, but I'm still not 100% sure about how the Serge integration actually works - still tracking down some of those details. For one, I don't know if the last `run_command` will actually generate and commit the files from Serge back to the repo, or...? But yeah, still looking.

---
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/lang/build/en.js
@@ -0,0 +1,10 @@
+export const translations = JSON.parse(`{
+  "name": {
+    "translation": "Name",
+    "context": "Helpful hints to make the Serge translation better"
+  },
+  "emptyNameError": {
+    "translation": "Name is required",
+    "context": "Helpful hints to make the Serge translation better"
+  }
+}`);
---
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/lang/build/fr.js
@@ -0,0 +1,10 @@
+export const translations = JSON.parse(`{
+  "name": {
+    "translation": "Nom",
+    "context": "Helpful hints to make the Serge translation better"
+  },
+  "emptyNameError": {
+    "translation": "Le nom est requis",
+    "context": "Helpful hints to make the Serge translation better"
+  }
+}`);

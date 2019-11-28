8adda279f67b74c114f73ea30d644da61ec72c37
Patrick Paskaris <Patrick.Paskaris@D2L.com>
2019-11-28 18:16:56 -0500

    Add release conditions dialog launcher

--- components/d2l-activity-editor/lang/en.js
+++ components/d2l-activity-editor/lang/en.js
@@ -1,6 +1,7 @@
 /* eslint quotes: 0 */
 
 export default {
+	"btnEditReleaseConditions": "Edit Release Conditions", // edit release conditions button
 	"dueDate": "Due Date", // ARIA label for the due date field when creating/editing an activity
 	"dueTime": "Due Time", // ARIA label for the due time field when creating/editing an activity
 	"hidden": "Hidden", // Label displayed with the visibility switch when hidden

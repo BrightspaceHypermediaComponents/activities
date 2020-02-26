bb90f594ef845dc9674d2ac24985beb930aeb978
Patrick Paskaris <Patrick.Paskaris@D2L.com>
2020-02-26 15:41:37 -0500

    devops

--- components/d2l-activity-editor/d2l-activity-conditions-editor.js
+++ components/d2l-activity-editor/d2l-activity-conditions-editor.js
@@ -1,6 +1,6 @@
 import 'd2l-dropdown/d2l-dropdown-button-subtle.js';
 import 'd2l-dropdown/d2l-dropdown-menu.js';
-import { css, html, LitElement } from 'lit-element/lit-element';
+import { css, html } from 'lit-element/lit-element';
 import { ActivityEditorMixin } from './mixins/d2l-activity-editor-mixin.js';
 import { getLocalizeResources } from './localization';
 import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';

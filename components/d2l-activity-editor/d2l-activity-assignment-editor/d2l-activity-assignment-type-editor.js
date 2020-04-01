e22617d565e0a13524b0bf47407cbb4193ad98ac
Uzair Shamim <Uzair.Shamim@D2L.com>
2020-04-01 16:15:34 -0400

    Modified assignements to differentiate between read only and has submissions

--- components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-type-editor.js
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-type-editor.js
@@ -96,7 +96,7 @@ class AssignmentTypeEditor extends ActivityEditorMixin(LocalizeMixin(MobxLitElem
 
 		const isIndividualAssignmentType = assignment.isIndividualAssignmentType;
 
-		if (assignment.isReadOnly) {
+		if (assignment.hasSubmissions) {
 			return this.localize('folderTypeCannotChange');
 		}
 
@@ -120,7 +120,7 @@ class AssignmentTypeEditor extends ActivityEditorMixin(LocalizeMixin(MobxLitElem
 
 		const isIndividualType = assignment.isIndividualAssignmentType;
 		const infoText = this._getInformationText(assignment);
-		const isReadOnly = assignment.isReadOnly;
+		const isReadOnly = assignment.isReadOnly || assignment.hasSubmissions;
 		const groupTypeDisabled = assignment.isGroupAssignmentTypeDisabled;
 		const folderTypeText =	isIndividualType ? this.localize('txtIndividual') : this.localize('txtGroup');
 		const groupTypeText = !isIndividualType && assignment.selectedGroupCategoryName

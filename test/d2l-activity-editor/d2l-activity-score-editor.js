e6d9911f1de853d0a36db8f3455f170b9921576b
Margaret Dulat <Margaret.Dulat@D2L.com>
2020-10-06 09:22:30 -0700

    add debug statements

--- test/d2l-activity-editor/d2l-activity-score-editor.js
+++ test/d2l-activity-editor/d2l-activity-score-editor.js
@@ -12,7 +12,7 @@ function dispatchEvent(elem, eventType, composed) {
 	elem.dispatchEvent(e);
 }
 
-describe.skip('d2l-activity-score-editor', function() {
+describe('d2l-activity-score-editor', function() {
 
 	let el, href, activity, score;
 
@@ -102,7 +102,7 @@ describe.skip('d2l-activity-score-editor', function() {
 		});
 	});
 
-	describe('events', () => {
+	describe.only('events', () => {
 		it('updates score out of', async() => {
 			const input = el.shadowRoot.querySelector('#score-out-of');
 			input.value = '15';

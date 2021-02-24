f2a00add4902473211fd282aaf2e3ab43f502050
Danny Gleckler <daniel.gleckler@d2l.com>
2021-02-24 11:35:06 -0500

    Remove supportedLangs

--- components/d2l-work-to-do/mixins/d2l-work-to-do-localization-mixin.js
+++ components/d2l-work-to-do/mixins/d2l-work-to-do-localization-mixin.js
@@ -5,7 +5,6 @@ export const LocalizeWorkToDoMixin = superclass => class extends LocalizeDynamic
 	static get config() {
 		return {
 			importFunc: lang => import(`../lang/${lang}.js`),
-			supportedLangs: ['ar', 'cy-gb', 'cy', 'da', 'de', 'en', 'en-us', 'es-es', 'es', 'fr-ca', 'fr', 'ja', 'ko', 'nl', 'pt', 'sv', 'tr', 'zh-tw', 'zh'],
 			osloCollection: 'd2l-activities\\workToDo',
 			exportName: 'val',
 		};

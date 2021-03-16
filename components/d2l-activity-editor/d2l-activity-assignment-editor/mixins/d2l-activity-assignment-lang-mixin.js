51181064d9bc7a9728c17407b822864264f27a82
Kieran Anderson <Kieran.Anderson@D2L.com>
2021-03-16 13:33:40 -0400

    Linter fixes

--- components/d2l-activity-editor/d2l-activity-assignment-editor/mixins/d2l-activity-assignment-lang-mixin.js
+++ components/d2l-activity-editor/d2l-activity-assignment-editor/mixins/d2l-activity-assignment-lang-mixin.js
@@ -1,7 +1,7 @@
 import { LocalizeDynamicMixin } from '@brightspace-ui/core/mixins/localize-dynamic-mixin.js';
 
 export const LocalizeActivityAssignmentEditorMixin = superclass => class extends LocalizeDynamicMixin(superclass) {
-	 
+
 	static get localizeConfig() {
 		return {
 			importFunc: async lang => (await import(`../lang/${lang}.js`)).default,

ab2ad8275376fe40272610451443d0d5d42ff3dc
ianlaidlaw <laidlaw434@gmail.com>
2021-03-23 12:47:56 -0500

    use is defined

--- components/d2l-activity-editor/d2l-activity-html-new-editor.js
+++ components/d2l-activity-editor/d2l-activity-html-new-editor.js
@@ -1,7 +1,9 @@
 import '@brightspace-ui/htmleditor/htmleditor.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { ActivityEditorMixin } from './mixins/d2l-activity-editor-mixin.js';
+import { ifDefined } from 'lit-html/directives/if-defined';
 import { LocalizeActivityEditorMixin } from './mixins/d2l-activity-editor-lang-mixin.js';
+
 class ActivityHtmlNewEditor extends ActivityEditorMixin(LocalizeActivityEditorMixin(LitElement)) {
 
 	static get properties() {
@@ -10,9 +12,9 @@ class ActivityHtmlNewEditor extends ActivityEditorMixin(LocalizeActivityEditorMi
 			ariaLabel: { type: String },
 			disabled: { type: Boolean },
 			_filesToReplace: { type: Object },
-			htmlEditorHeight: { type: String },
-			fullPage: { type: Boolean },
-			fullPageFontSize: { type: String }
+			htmlEditorHeight: { type: String, attribute: 'html-editor-height' },
+			fullPage: { type: Boolean, attribute: 'full-page' },
+			fullPageFontSize: { type: String, attribute: 'full-page-font-size' }
 		};
 	}
 
@@ -31,6 +33,7 @@ class ActivityHtmlNewEditor extends ActivityEditorMixin(LocalizeActivityEditorMi
 		this._context = JSON.parse(document.documentElement.getAttribute('data-he-context'));
 		this._filesToReplace = {};
 		this.saveOrder = 500;
+		this.fullPage = false;
 	}
 
 	render() {
@@ -42,9 +45,9 @@ class ActivityHtmlNewEditor extends ActivityEditorMixin(LocalizeActivityEditorMi
 				label="${this.ariaLabel}"
 				label-hidden
 				?disabled="${this.disabled}"
-				height="${this.htmlEditorHeight}"
+				height=${ifDefined(this.htmlEditorHeight)}
 				?full-page="${this.fullPage}"
-				full-page-font-size="${this.fullPageFontSize}"
+				full-page-font-size="${ifDefined(this.fullPageFontSize)}"
 				?paste-local-images="${allowPaste}">
 			</d2l-htmleditor>
 		`;

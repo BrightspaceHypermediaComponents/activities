8a1a8e171e621ab558494fee8f003448c83ff9c5
brissons <scott.brisson@gmail.com>
2021-05-05 17:26:24 -0500

    allow saving (will need refactoring)

--- components/d2l-activity-editor/d2l-activity-content-editor/content-file/state/content-file.js
+++ components/d2l-activity-editor/d2l-activity-content-editor/content-file/state/content-file.js
@@ -30,10 +30,11 @@ export class ContentFile {
 		const sirenEntity = await fetchEntity(this.href, this.token);
 		if (sirenEntity) {
 			let entity = new ContentFileEntity(sirenEntity, this.token, { remove: () => { } });
-			entity = await this._checkout(entity);
+
 			if (entity.getFileType() === "html") {
-				entity = new ContentHtmlFileEntity(sirenEntity, this.token, { remove: () => { } });
-				
+				entity = new ContentHtmlFileEntity(entity, this.token, { remove: () => { } });
+				entity = await this._checkout(entity);
+	
 				const fileEntityHref = await fetchEntity(entity.getFileHref(), this.token);
 				const fileEntity = new FileEntity(fileEntityHref, this.token, { remove: () => { } });
 				const htmlContentFetchResponse = await fetch(fileEntity.getFileLocationHref()); 
@@ -49,10 +50,7 @@ export class ContentFile {
 	load(contentFileEntity, htmlContent = null) {
 		this._contentFile = contentFileEntity;
 		this.title = contentFileEntity.title();
-		this.fileType = contentFileEntity.getFileType();
-		if (htmlContent) {
-			this.htmlContent = htmlContent;
-		}
+		this.htmlContent = htmlContent;
 	}
 
 	async save() {
@@ -61,10 +59,8 @@ export class ContentFile {
 		}
 
 		await this._contentFile.setFileTitle(this.title);
+		await this._contentFile.setHtmlFileHtmlContent(this.htmlContent);
 		
-		if (this.fileType === "html") {
-			await this._contentFile.setHtmlFileHtmlContent(this.htmlContent);
-		}
 
 		const committedContentFileEntity = await this._commit(this._contentFile);
 		const editableContentFileEntity = await this._checkout(committedContentFileEntity);
@@ -88,7 +84,7 @@ export class ContentFile {
 		if (!sirenEntity) {
 			return contentFileEntity;
 		}
-		return new ContentFileEntity(sirenEntity, this.token, { remove: () => { } });
+		return new ContentHtmlFileEntity(sirenEntity, this.token, { remove: () => { } });
 	}
 
 	async _commit(contentFileEntity) {
@@ -100,7 +96,8 @@ export class ContentFile {
 		if (!sirenEntity) {
 			return contentFileEntity;
 		}
-		return new ContentFileEntity(sirenEntity, this.token, { remove: () => { } });
+		
+		return new ContentHtmlFileEntity(sirenEntity, this.token, { remove: () => { } });
 	}
 
 	_makeContentFileData() {

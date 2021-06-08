d01605711fc412556e0fa409d9d7519f440f8a99
Diljot Garcha <Diljot.Garcha@D2L.com>
2021-06-08 16:03:33 -0500

    Fix the styles of the jump icon to match the design better

--- components/d2l-activity-editor/d2l-activity-content-editor/lti-link/d2l-activity-content-lti-link-jump-icon.js
+++ components/d2l-activity-editor/d2l-activity-content-editor/lti-link/d2l-activity-content-lti-link-jump-icon.js
@@ -1,5 +1,5 @@
 import { css, html } from 'lit-element/lit-element.js';
-import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
+import { bodyStandardStyles } from '@brightspace-ui/core/components/typography/styles.js';
 import { LocalizeActivityEditorMixin } from '../../mixins/d2l-activity-editor-lang-mixin.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
@@ -29,7 +29,7 @@ class ActivityContentLTILinkJumpIcon extends LocalizeActivityEditorMixin(RtlMixi
 					width: 50%;
 				}
 			`,
-			labelStyles
+			bodyStandardStyles
 		];
 	}
 
@@ -54,7 +54,7 @@ class ActivityContentLTILinkJumpIcon extends LocalizeActivityEditorMixin(RtlMixi
 					</svg>
 				</div>
 				<slot>
-					<p class="d2l-body-small">
+					<p class="d2l-body-standard">
 						${this.text}
 					</p>
 				</slot>
